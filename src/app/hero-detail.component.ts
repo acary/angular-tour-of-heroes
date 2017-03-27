4cf7b899011ecb8e8d5b85946a9efc70ed637595
acary <andy@andycary.com>
2017-03-27 09:38:23 -0700

    create data service for heroes

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -9,12 +9,13 @@ import {Hero} from './hero';
 @Component({
     selector: 'my-hero-detail',
     templateUrl: './app/hero-detail.component.html',
+    styleUrls: [ './app/hero-detail.component.css' ]
 })
 
 export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
-
+    
     constructor(
         private heroService: HeroService,
         private route: ActivatedRoute,
