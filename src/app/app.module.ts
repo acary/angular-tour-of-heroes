7739fc114f9a9184d09f2bf086211e8db2ce96e6
acary <andy@andycary.com>
2017-03-27 11:53:57 -0700

    completed tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
     imports: [
@@ -23,7 +24,7 @@ import { HeroService }          from './hero.service';
         InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
-    declarations: [ AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent ],
+    declarations: [ AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent, HeroSearchComponent ],
     providers:    [ HeroService ],
     bootstrap:    [ AppComponent ],
 })
